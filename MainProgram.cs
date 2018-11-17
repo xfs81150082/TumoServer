57ad9b2e1288484634c45ef8b1402150c9e58127
图末 <44885824+xfs81150082@users.noreply.github.com>
2018-11-17 13:20:31 +0800

    Add files via upload

---
+++ MainProgram.cs
@@ -0,0 +1,24 @@
+﻿using Servers.Gates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servers.Peers
+{
+    class MainProgram
+    { 
+        //程序启动入口
+        static void Main(string[] args)
+        {
+            new PeerHelper();
+            new MysqlHelper();
+            new TumoGate();
+            new SocketListen();
+
+            Console.ReadKey();
+            Console.WriteLine("退出监听，并关闭程序。");
+        }
+    }
+}
