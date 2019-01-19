df0f3e543ff829c1e14f16149205cbec88843b0c
xfs81150082 <81150082@qq.com>
2019-01-20 01:02:50 +0800

    2019.1.20  ECS

---
+++ Servers/TmGames/Systems/TmDelegate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tumo;
+
+namespace Servers
+{
+    public class TmDelegate : TmComponent
+    {
+        private static TmDelegate _instance;
+        public static TmDelegate Instance { get => _instance; }
+        public override void TmAwake()
+        {
+            base.TmAwake();
+            _instance = this;
+        }
+        public delegate void TmLoginParameterEvent(TmParameter parameter);
+        public delegate void TmEngineerParameterEvent(TmParameter parameter);
+        public delegate void TmBookerParameterEvent(TmParameter parameter);
+        public delegate void TmTeacherParameterEvent(TmParameter parameter);
+        public delegate void TmInventoryParameterEvent(TmParameter parameter);
+        public delegate void TmSkillParameterEvent(TmParameter parameter);
+        public delegate void TmTaskParameterEvent(TmParameter parameter);
+        public delegate void TmWarParameterEvent(TmParameter parameter);
+        public delegate void TmSenceParameterEvent(TmParameter parameter);
+    }
+}
---
+++ Servers/TmMain/TmServerMain.cs
@@ -0,0 +1,30 @@
+﻿using Servers.Games;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Tumo;
+
+namespace Servers
+{
+    class TmServerMain
+    { 
+        //程序启动入口
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TmTimerTool.CurrentTime() + " ... ");
+            ///创建游戏服务器并运行
+            new GameServer();
+
+            Thread.CurrentThread.Name = "TumoWorld";
+            Console.WriteLine(TmTimerTool.CurrentTime() + " ThreadName:" + Thread.CurrentThread.Name);
+            Console.WriteLine(TmTimerTool.CurrentTime() + " ThreadId:" + Thread.CurrentThread.ManagedThreadId);
+
+            Console.ReadKey();
+            Console.WriteLine(TmTimerTool.CurrentTime() + " 退出监听，并关闭程序。");
+        }
+    }
+}
