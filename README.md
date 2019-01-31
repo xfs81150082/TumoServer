15f6c5197307225144c9563042acbee3c7762a0d
xfs81150082 <81150082@qq.com>
2019-01-31 21:38:11 +0800

    2019.1.31-33

--- README.md
+++
@@ -1,5 +0,0 @@
-# TumoServer
-TumoWorld 服务器，C# Console，Socket 异步 多线程。
-
-
-[UpdateBefore(typeof(RotationSpeedSystem))]
--- Servers/Games/Models/Rolers/TmEngineer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using Tumo;
-namespace Servers
-{ 
-    class TmEngineer : TmComponent
-    {
-    }
-}
