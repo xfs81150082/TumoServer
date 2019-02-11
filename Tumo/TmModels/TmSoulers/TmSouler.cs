ad053820be57890dc18159bbd85ea152d2b236a9
xfs81150082 <81150082@qq.com>
2019-02-11 20:29:58 +0800

    2019.2.11-1

--- Tumo/TmModels/TmSoulers/TmSouler.cs
+++ Tumo/TmModels/TmSoulers/TmSouler.cs
@@ -23,31 +23,31 @@ namespace Tumo
         public bool Start { get; set; } = false;
         public bool End { get; set; } = false;
 
-        public int Stamina { get; set; } = 0;            //耐力，案例，每升1级加1，影响血量
-        public int Brains { get; set; } = 0;             //智力，计价，每升1级加1，影响魔法攻击强度，魔法防御
-        public int Power { get; set; } = 0;              //力量，计量，每升1级加1，影响物理攻击强度，物理防御
-        public int Agility { get; set; } = 0;            //敏捷，管理，每升1级加1，影响暴击率，命中率，闪避率 
-        public int Hp { get; set; } = 0;                 //当前血量
+        public int Stamina { get; set; } = 10;            //耐力，案例，每升1级加1，影响血量
+        public int Brains { get; set; } = 10;             //智力，计价，每升1级加1，影响魔法攻击强度，魔法防御
+        public int Power { get; set; } = 10;              //力量，计量，每升1级加1，影响物理攻击强度，物理防御
+        public int Agility { get; set; } = 10;            //敏捷，管理，每升1级加1，影响暴击率，命中率，闪避率 
+        public int Hp { get; set; } = 10;                 //当前血量
         public int Mp { get; set; } = 0;                 //当前血量
-        public int MaxHp { get; set; } = 0;              //HP=10 * 耐力 + 装备 + 法术；暂定10倍；
-        public int MaxMp { get; set; } = 0;              //MP=10 * Level;
+        public int MaxHp { get; set; } = 100;            //HP=10 * 耐力 + 装备 + 法术；暂定10倍；
+        public int MaxMp { get; set; } = 10;             //MP=10 * Level;
         public int Bp { get; set; } = 0;                 //Bp与智力成正比；暂定1倍；
         public int Ap { get; set; } = 0;                 //Ap与力量成正比；暂定1倍；
-        public double Hr { get; set; } = 0.0f;           //hr命中率与敏捷成正比。
-        public double Cr { get; set; } = 0.0f;           //cr暴击率与敏捷成正比。
-        public double Sp { get; set; } = 0.0f;           //移动速度,与敏捷成正比。 
-        public double StaminaRate { get; set; } = 0.0f;  //耐力，案例，每升1级加1，影响血量
-        public double BrainsRate { get; set; } = 0.0f;   //智力，计价，每升1级加1，影响魔法攻击强度，魔法防御
-        public double PowerRate { get; set; } = 0.0f;    //力量，计量，每升1级加1，影响物理攻击强度，物理防御
-        public double AgilityRate { get; set; } = 0.0f;  //敏捷，管理，每升1级加1，影响暴击率，命中率，闪避率 
-        public double HpRate { get; set; } = 0.0f;       //当前血量
-        public double MpRate { get; set; } = 0.0f;       //当前血量
-        public double MaxHpRate { get; set; } = 0.0f;    //HP=10 * 耐力 + 装备 + 法术；暂定10倍；
-        public double MaxMpRate { get; set; } = 0.0f;    //MP=10 * Level;
-        public double ApRate { get; set; } = 0.0f;       //Ap与力量成正比；暂定1倍；
-        public double BpRate { get; set; } = 0.0f;       //Bp与智力成正比；暂定1倍；
-        public double HrRate { get; set; } = 0.0f;       //hr命中率与敏捷成正比。
-        public double CrRate { get; set; } = 0.0f;       //cr暴击率与敏捷成正比。
-        public double SpRate { get; set; } = 0.0f;       //移动速度        
+        public double Hr { get; set; } = 0.20;           //hr命中率与敏捷成正比。
+        public double Cr { get; set; } = 0.40;           //cr暴击率与敏捷成正比。
+        public double Sp { get; set; } = 2.0;            //移动速度,与敏捷成正比。 
+        public double StaminaRate { get; set; } = 10.0;  //耐力，案例，每升1级加1，影响血量
+        public double BrainsRate { get; set; } = 1.0;    //智力，计价，每升1级加1，影响魔法攻击强度，魔法防御
+        public double PowerRate { get; set; } = 1.0;    //力量，计量，每升1级加1，影响物理攻击强度，物理防御
+        public double AgilityRate { get; set; } = 1.0;  //敏捷，管理，每升1级加1，影响暴击率，命中率，闪避率 
+        public double HpRate { get; set; } = 0.0;       //当前血量
+        public double MpRate { get; set; } = 0.0;       //当前血量
+        public double MaxHpRate { get; set; } = 0.0;    //HP=10 * 耐力 + 装备 + 法术；暂定10倍；
+        public double MaxMpRate { get; set; } = 0.0;    //MP=10 * Level;
+        public double ApRate { get; set; } = 0.0;       //Ap与力量成正比；暂定1倍；
+        public double BpRate { get; set; } = 0.0;       //Bp与智力成正比；暂定1倍；
+        public double HrRate { get; set; } = 0.0;       //hr命中率与敏捷成正比。
+        public double CrRate { get; set; } = 0.0;       //cr暴击率与敏捷成正比。
+        public double SpRate { get; set; } = 0.0;       //移动速度        
     }
 }
--- Tumo/TmModels/TmSoulers/TmSoulerItem.cs
+++ Tumo/TmModels/TmSoulers/TmSoulerItem.cs
@@ -25,46 +25,16 @@ namespace Tumo
                 RemoveComponent<TmSoulerDB>();
             }
             AddComponent(itemDB);
+            (this.GetComponent<TmName>() as TmName).Name = (this.GetComponent<TmSoulerDB>() as TmSoulerDB).Name;
+            (this.GetComponent<TmName>() as TmName).Id = (this.GetComponent<TmSoulerDB>() as TmSoulerDB).Id;
+            (this.GetComponent<TmName>() as TmName).ParentId = (this.GetComponent<TmSoulerDB>() as TmSoulerDB).UserId;
+            (this.GetComponent<TmChangeType>() as TmChangeType).Exp = (this.GetComponent<TmSoulerDB>() as TmSoulerDB).Exp;
+            (this.GetComponent<TmChangeType>() as TmChangeType).Level = (this.GetComponent<TmSoulerDB>() as TmSoulerDB).Level;
+            (this.GetComponent<TmChangeType>() as TmChangeType).Coin = (this.GetComponent<TmSoulerDB>() as TmSoulerDB).Coin;
+            (this.GetComponent<TmChangeType>() as TmChangeType).Diamond = (this.GetComponent<TmSoulerDB>() as TmSoulerDB).Diamond;
+            (this.GetComponent<TmProperty>() as TmProperty).Hp = (this.GetComponent<TmSoulerDB>() as TmSoulerDB).Hp;
+            (this.GetComponent<TmProperty>() as TmProperty).Mp = (this.GetComponent<TmSoulerDB>() as TmSoulerDB).Mp;
         }
-   
-        //public void InitAttribute()
-        //{
-        //    UpdateLevel();
-        //    Stamina = (Soul.Stamina + Level);
-        //    Brains = (Soul.Brains + Level);
-        //    Power = (Soul.Power + Level);
-        //    Agility = (Soul.Agility + Level);
-        //    MaxHp = (int)Math.Round(Stamina * Soul.StaminaRate);                            //Hp与根骨成正比；暂定10倍；
-        //    MaxMp = (int)Math.Round(Level * Soul.StaminaRate);                              //Mp与等级有关；暂定10倍；
-        //    Bp = (int)Math.Round(Brains * Soul.BrainsRate);                                 //Bp与智力成正比；暂定1倍；
-        //    Ap = (int)Math.Round(Power * Soul.PowerRate);                                   //Ap与力量成正比；暂定1倍；
-        //    Hr = Math.Round((Soul.Hr + (Agility * Soul.AgilityRate) / (100 * Level)), 4);   //hr命中率与敏捷成正比。
-        //    Cr = Math.Round((Soul.Cr + (Agility * Soul.AgilityRate) / (100 * Level)), 4);   //cr暴击率与敏捷成正比。
-        //    Sp = Math.Round((Soul.Sp + (Agility * Soul.AgilityRate) / (100 * Level)), 4);
-        //} //更新属性
-        //void UpdateLevel()
-        //{
-        //    if (Level == Soul.LevelUpLimit) return;
-        //    int expTem = (int)Math.Round((Level + 1.0) * (Level + 1.0) + 10.0);
-        //    if (Exp >= expTem)
-        //    {
-        //        Exp -= expTem;
-        //        Level++;
-        //        if (Level >= Soul.LevelUpLimit)
-        //        {
-        //            Level = Soul.LevelUpLimit;
-        //            Exp = 0;
-        //        }
-        //        this.Hp = this.MaxHp;
-        //        this.Mp = this.MaxMp;
-        //    }
-        //    //1-59级：经验值 = ((8 × 角色等级) + 难度系数(角色等级)) × 基础经验值(角色等级) × 经验系数(角色等级)
-        //    //60级：经验值 = 155 + 基础经验值(角色等级) × (1275 - ((69 - 角色等级) × (3 + (69 - 角色等级) × 4)));
-        //    //61-69级：经验值 = 155 + 基础经验值(角色等级) × (1344 - ((69 - 角色等级) × (3 + (69 - 角色等级) × 4)));
-        //    //基础经验值(角色等级) = 45 + (5 × 角色等级) (适用于艾泽拉斯的怪物) 基础经验值(角色等级) = 235 + (5 × 角色等级) (适用于外域的怪物) 基础经验值(角色等级) = 626 + (5 × 角色等级) (适用于诺森德的怪物);
-        //    //难度系数(角色等级) = 0 (角色等级 <= 28) 难度系数(角色等级) = 1 (角色等级 = 29) 难度系数(角色等级) = 3 (角色等级 = 30) 难度系数(角色等级) = 6 (角色等级 = 31) 难度系数(角色等级) = 5 × (角色等级 - 30) (角色等级 >= 32, <= 59);
-        //    //经验系数(角色等级) = 1(角色等级 <= 10) 经验系数(角色等级) = (1 - (角色等级 - 10) / 100)(角色等级 >= 11, <= 27) 经验系数(角色等级) = 0.82(角色等级 >= 28, <= 59);
-        //} //等级更新        
 
     }
 }
