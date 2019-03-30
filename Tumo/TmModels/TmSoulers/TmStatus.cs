b7b6be9ca1ede3a128d374b8e0a8b9f9f063cd39
xfs81150082 <81150082@qq.com>
2019-03-30 11:08:34 +0800

    2019.3.30-2

--- Tumo/TmModels/TmSoulers/TmStatus.cs
+++ Tumo/TmModels/TmSoulers/TmStatus.cs
@@ -7,11 +7,12 @@ namespace Tumo
     public class TmStatus
     {
         public int RoelerId;
+        public string Name;
         public string Key;
         public string CurdateTime;              //当前时间数值
         public string AttackState;              //攻击动画状态
         public double MoveSpeed;                //当前速度
         public TmTransform MyselfTmTransform;   //当前坐标
-        public TmTransform TargetTmTransform;   //当前目标坐标或朝向
+        public TmTransform TargetTmTransform;   //当前目标坐标
     }
 }
